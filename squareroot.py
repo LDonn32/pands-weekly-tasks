fda58780f63d8646588a9e6e3e6ed70834739414
LDonn32 <lauradonnelly32@gmail.com>
2025-03-13 20:05:23 +0000

    first commit

--- squareroot.py
+++ squareroot.py
@@ -4,3 +4,8 @@
 # and outputs an approximation of its square root.
 
 # Author: Laura Donnelly
+
+# Resources: 
+
+# https://en.wikipedia.org/wiki/Newton%27s_method
+# https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
