deb24a492a2a9f9eb72fe5c3dcaaa6a4af6c1bad
LDonn32 <lauradonnelly32@gmail.com>
2025-03-21 15:20:53 +0000

    updated with ref list and comments

--- banks.py
+++ banks.py
@@ -14,10 +14,20 @@ amount1 =float(input("Enter amount of money in cent:"))
 #prompt user for second amount of money in cents
 amount2 = float(input("Enter a second amount of money in cents:"))
 
+# input function will asks the user in the terminal to input the money.
+# we are asking them to do so in cents
+# however we need to convert the money into cents in the code also
+# we can do so using the float function
+#[Link] https://www.geeksforgeeks.org/taking-input-from-console-in-python/
+#[Link] https://www.geeksforgeeks.org/float-in-python/
+
 # Add the two amounts and divide by 100 for decimal
 Sum = (amount1 + amount2)/100
 
-# print with a F string to include the euro sign 
-# https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
+# This calculation is then stored as a variable in Sum
+# when this is printed later it should show as decimal to properly count the cents if the input is large enough to convert into euros
 
-print(f"€{Sum}")
+# print with a F string to include the euro sign
+print(f"The sum of these is €{Sum}")
+
+#[Link] https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
