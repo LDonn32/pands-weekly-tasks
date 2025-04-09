decc538e4b7e84a15fdf6132fdea4849a4a0cd1e
LDonn32 <lauradonnelly32@gmail.com>
2025-04-09 17:57:51 +0100

    tes t

--- accounts.py
+++ accounts.py
@@ -1,7 +1,14 @@
-# This programme reads in a 10 character account number 
-# outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs).
+# Weekly Task 3 
 # Author: Laura Donnelly
 
+# Bank account numbers can stored as 10 character strings, for security reasons some applications only display the last 4 characters (with the other other characters replaced with Xs).
+# Write a python program called accounts.py that reads in a 10 character account number and outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs). 
+# $ python accounts.py
+# Please enter an 10 digit account number: 1234567890
+# XXXXXX7890
+
+# Extra:
+# Modify the program to deal with account numbers of any length (yes that is a vague requirement, comment your assumptions)
 
 # resources: 
 # [Link] https://docs.python.org/3/library/string.html
@@ -12,8 +19,11 @@
 
 # [Link] https://www.w3schools.com/python/python_conditions.asp
 
+
+
+# Define the function.
 def mask_account_number():
-    # Read the account number
+    # Read in the account number
     account_number = input("Please enter your 10-character account number: ")
     
     # Check if the input length is 10 characters
