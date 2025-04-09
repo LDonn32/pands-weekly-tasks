f7b36e17fae31e034533b86d46caa4b61653cfd3
LDonn32 <lauradonnelly32@gmail.com>
2025-04-09 15:57:53 +0100

    updated

--- banks.py
+++ banks.py
@@ -1,11 +1,12 @@
+# Weekly Task 02
+# Author: Laura Donnelly
+
+
 # banks.py
 # this programme should do the following:
 # prompts the user and read in two money amounts (in cent)
 # Add the two amounts 
 # Print out the answer in a human readable format with the euro sign and decimal point and cent of the amount
-
-# Author: Laura Donnelly
-# Weekly Task 02
 
 
 # prompt user for first amount of money in cents
@@ -15,11 +16,11 @@ amount1 =float(input("Enter amount of money in cent:"))
 amount2 = float(input("Enter a second amount of money in cents:"))
 
 # input function will asks the user in the terminal to input the money.
-# we are asking them to do so in cents
-# however we need to convert the money into cents in the code also
-# we can do so using the float function
-#[Link] https://www.geeksforgeeks.org/taking-input-from-console-in-python/
-#[Link] https://www.geeksforgeeks.org/float-in-python/
+# I am asking them to do so in cents
+# however I need to convert the money into cents in the code also
+# I can do so using the float function
+#[Reference] https://www.geeksforgeeks.org/taking-input-from-console-in-python/
+#[Reference] https://www.geeksforgeeks.org/float-in-python/
 
 # Add the two amounts and divide by 100 for decimal
 Sum = (amount1 + amount2)/100
