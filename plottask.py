03916e1b3c4db83b8b4a9bd22aa2666272335eaf
LDonn32 <lauradonnelly32@gmail.com>
2025-04-23 13:49:30 +0100

    tidy up comments and test

--- plottask.py
+++ plottask.py
@@ -1,59 +1,35 @@
-# Weekly Task 8 
+# plottask.py 
+# Weekly Task 08 
+
 # Author: Laura Donnelly
 
-# Write a program called plottask.py that displays:
-# a histogram of a normal distrubtion of a 1000 value with a mean of 5 and standard deviation of 2,
-# and a plot of the function h(x)=x3 in the range 0-10
-# on the set of axes
-# include legends if possible
-# copy the .png file into the repository.
-
-
-# Rescoures:
-# [Link] https://numpy.org/doc/stable/reference/random/generated/numpy.random.normal.html
-# [Link] https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.hist.html
-# [Link] https://numpy.org/doc/stable/reference/generated/numpy.arange.html
-# [Link] https://www.geeksforgeeks.org/numpy-arrange-in-python/
-# [Link] https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
-# [Link] https://www.geeksforgeeks.org/matplotlib-pyplot-legend-in-python/
-# [Link] https://www.geeksforgeeks.org/matplotlib-pyplot-tight_layout-in-python/
 
 # Import packages.
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Create a normal distribution with the information given in the task: 
-# 1000 values, mean=5, std=2.
-
+# Create a normal distribution.
 mean = 5
 std_dev = 2
 num_samples = 1000
 
 # Generate the random data.
-# [Link] https://numpy.org/doc/stable/reference/random/generated/numpy.random.normal.html
-
 data = np.random.normal(mean, std_dev, num_samples)
 
 # Plot the histogram.
-# [Link] https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.hist.html
-
 # 1 row, 2 columns, first subplot.
 plt.subplot(1, 2, 1)  
+
 plt.hist(data, bins=30, color='pink', edgecolor='purple', label='Normal Distribution')
 plt.title('Histogram of Normal Distribution')
 plt.xlabel('Value')
 plt.ylabel('Frequency')
 
 # Add a legend to the histogram.
-# [Link] https://www.geeksforgeeks.org/matplotlib-pyplot-legend-in-python/
 
 plt.legend(loc='upper left')
 
 # Plot the function h(x) = x^3 using np.arange().
-# [Link] https://numpy.org/doc/stable/reference/generated/numpy.arange.html
-# [Link] https://www.geeksforgeeks.org/numpy-arrange-in-python/
-
 # Create x values from 0 to 10 with a step of 0.1.
 x_values = np.arange(0, 10, 0.1)  
 
@@ -61,8 +37,6 @@ x_values = np.arange(0, 10, 0.1)
 y_value = x_values**3  
 
 # Create the function plot.
-# [Link] https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
-
 # 1 row, 2 columns, second subplot.
 plt.subplot(1, 2, 2)  
 
@@ -76,14 +50,10 @@ plt.legend(loc='upper left' )
 
 
 # Save the histogram and plot.
-# Note: I had to change the flow of plt.show() and plt.savefig()
-# If I ran show first, the saved file would come up blank
-# They were interferring with each other
 plt.savefig('plottask.png')
 
 
 # Adjust layout to avoid any overlap of the histogram or plot.
-# [Link] https://www.geeksforgeeks.org/matplotlib-pyplot-tight_layout-in-python/
 plt.tight_layout()
 
 # Display the plots.
